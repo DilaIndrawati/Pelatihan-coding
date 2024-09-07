1f8560593cd3c130f249a6e701f4c028cc73f7d1
DilaIndrawati <dila.indrawati7@gmail.com>
2024-09-07 11:06:06 +0700

    Add files via upload

---
+++ PELATIHAN CODING/DAY1.py
@@ -0,0 +1,36 @@
+# a = "apel" # "a" adalah variable dan "apel " adalah data
+# print(a)
+
+# buah = "jeruk" #ini adalah tipedata'string'
+# harga = 15000 #ini adalah tipedata 'integer'
+# berat = 1.5 #ini adalah tipedata 'float'
+# beli = True #ini adalah tipe data 'boolen'
+
+# print(buah)
+# print(harga)
+# print(berat)
+# print(beli)
+
+# # format string
+# nama = 'siti'
+# umur = '16'
+# alamat = 'jl. buntu'
+# tinggi = '1.75'
+
+# print(f"nama saya {nama}. saya berusia {umur} tahun, dan tinggi saya {tinggi} meter. saya tinggal di {alamat}")
+
+# # input
+# nama = input ("siapa  nama kamu?")
+# print(f"hallo, {nama}")
+
+# umur = int (input("umur kamu berapa?"))
+# print (f"{umur} tahun")
+
+print("halo, perkenalkan nama aku bot!")
+print("halo, bot!")
+nama = input("nama, kamu siapa?")
+print(f"namaku {nama}")
+print(F"halo, {nama}. salam kenal!")
+print(f"kamu sibuk apa sekarang, {nama}?")
+kegiatan = input("kesibukanmu?")
+print(F"oke, semoga {kegiatan} berhasil")
